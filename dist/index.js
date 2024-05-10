17708d1bf22205201136e7b48974e40df5d0757f
Lincoln Barr <lbarr@revenuewell.com>
2024-05-10 15:59:57 -0700

    Add test icon in dist folder

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+export { default as testIcon } from './library/testIcon';
---
+++ dist/library/testIcon.js
@@ -0,0 +1,9 @@
+const testIcon = (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<rect x="44" y="233" width="424" height="46"/>
+<rect x="102" y="68" width="306" height="120"/>
+<rect x="102" y="326" width="306" height="120"/>
+</svg>
+);
+export default testIcon;
